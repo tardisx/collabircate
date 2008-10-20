503543c20bde419ac87a15750507ed806463d4a2
justin <justin@8d8e6d39-c747-0410-aa99-ea999ebef502>
2008-10-20 05:09:43 +0000

    Reformat!
    git-svn-id: svn+ssh://romana.hawkins.id.au/home/justin/repos/CollabIRCate/trunk@96 8d8e6d39-c747-0410-aa99-ea999ebef502

--- bin/test.pl
+++ bin/test.pl
@@ -3,7 +3,8 @@ use strict;
 use warnings;
 use FindBin qw/$Bin/;
 use Path::Class;
-use lib dir($Bin, '..', 'lib')->stringify;
+use lib dir( $Bin, '..', 'lib' )->stringify;
+
 # use YAML qw/LoadFile/;
 
 use CollabIRCate::Schema;
@@ -11,6 +12,15 @@ use CollabIRCate::Schema::Channel;
 
 # my $config = LoadFile(file($Bin, '..', 'collabircate.conf'));
 
-my $schema = CollabIRCate::Schema->connect('dbi:Pg:dbname=collabircate') || dir $!;
+my $schema = CollabIRCate::Schema->connect('dbi:Pg:dbname=collabircate')
+  || die $!;
 
-my $channel = $schema->resultset('Channel')->create({ name => 'fred' });
+foreach ( 10 .. 99 ) {
+    my $channel = $schema->resultset('Channel')->create(
+        {
+            name        => '#channel' . $_,
+            description => 'Channel ' . $_
+        }
+    );
+    warn "Created " . $channel->channel_id;
+}
