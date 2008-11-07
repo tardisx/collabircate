5ab45a64058764955a8518997e658b10faefeaff
justin <justin@8d8e6d39-c747-0410-aa99-ea999ebef502>
2008-11-07 23:48:22 +0000

    Updated schema.
    git-svn-id: svn+ssh://romana.hawkins.id.au/home/justin/repos/CollabIRCate/trunk@117 8d8e6d39-c747-0410-aa99-ea999ebef502

--- etc/schema.sql
+++ etc/schema.sql
@@ -6,39 +6,39 @@ DROP TABLE users;
 
 
 CREATE TABLE channel (
-  channel_id  SERIAL NOT NULL PRIMARY KEY,
+  id          SERIAL NOT NULL PRIMARY KEY,
   name        TEXT NOT NULL,
   description TEXT
 );
 
 
 CREATE TABLE users (
-  user_id    SERIAL NOT NULL PRIMARY KEY,
+  id    SERIAL NOT NULL PRIMARY KEY,
   email      TEXT
 );
 
 CREATE TABLE log (
-  log_id      SERIAL NOT NULL PRIMARY KEY,
+  id      SERIAL NOT NULL PRIMARY KEY,
   ts          TIMESTAMP NOT NULL DEFAULT NOW(),
-  channel_id  INT NOT NULL REFERENCES channel(channel_id),
-  user_id     INT NOT NULL REFERENCES users(user_id),
+  channel_id  INT NOT NULL REFERENCES channel(id),
+  users_id     INT NOT NULL REFERENCES users(id),
   type        TEXT NOT NULL,
   entry       TEXT NOT NULL
 );
 
 CREATE TABLE tag (
-  tag_id SERIAL NOT NULL PRIMARY KEY,
+  id SERIAL NOT NULL PRIMARY KEY,
   name   TEXT,
-  log_id INT NOT NULL REFERENCES log(log_id)
+  log_id INT NOT NULL REFERENCES log(id)
 );
 
 
 CREATE TABLE channel_report (
-  user_id  INT NOT NULL REFERENCES users(user_id),
-  channel_id INT NOT NULL REFERENCES channel(channel_id),
+  users_id  INT NOT NULL REFERENCES users(id),
+  channel_id INT NOT NULL REFERENCES channel(id),
   report_expires TIMESTAMP,
   report_frequency   TEXT,
   report_last TIMESTAMP,
-  PRIMARY KEY (user_id, channel_id)
+  PRIMARY KEY (users_id, channel_id)
 );
 
--- lib/CollabIRCate/Schema.pm
+++ lib/CollabIRCate/Schema.pm
@@ -8,8 +8,8 @@ use base 'DBIx::Class::Schema';
 __PACKAGE__->load_classes;
 
 
-# Created by DBIx::Class::Schema::Loader v0.04005 @ 2008-10-21 11:43:42
-# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:SxUeCgvVslE3Kxc7xG5NrA
+# Created by DBIx::Class::Schema::Loader v0.04005 @ 2008-10-28 10:59:22
+# DO NOT MODIFY THIS OR ANYTHING ABOVE! md5sum:r09nzUzX8ykk+lGOG8D9Ow
 
 
 # You can replace this text with custom content, and it will be preserved on regeneration
