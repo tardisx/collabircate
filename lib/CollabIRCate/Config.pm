ace4e97196d667fb95125092e07af3202587f1cf
justin <justin@8d8e6d39-c747-0410-aa99-ea999ebef502>
2008-11-22 03:25:48 +0000

    die -> croak and more checking.
    git-svn-id: svn+ssh://romana.hawkins.id.au/home/justin/repos/CollabIRCate/trunk@148 8d8e6d39-c747-0410-aa99-ea999ebef502

--- lib/CollabIRCate/Config.pm
+++ lib/CollabIRCate/Config.pm
@@ -1,10 +1,15 @@
 package CollabIRCate::Config;
 
+use strict;
+use warnings;
+
 use FindBin qw/$Bin/;
 use Path::Class;
 use lib dir( $Bin, '..', 'lib' )->stringify;
 
 use Config::General;
+use Carp qw/croak/;
+
 use CollabIRCate::Schema;
 
 our $config;
@@ -14,6 +19,8 @@ sub config {
 
     $config = { Config::General->new("$Bin/../collabircate.conf")->getall }
         unless defined $config;
+
+    croak "cannot load config file" unless defined $config;
 
     return $config;
 
@@ -25,7 +32,7 @@ sub schema {
     my $config = config();
     
     $schema = CollabIRCate::Schema->connect($config->{dsn})
-        || die $!;
+        || croak "cannot connect to schema";
 
     return $schema;
 
