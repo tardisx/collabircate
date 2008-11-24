a2ec1a3264873334359d09635fc7669a14a2fb8e
justin <justin@8d8e6d39-c747-0410-aa99-ea999ebef502>
2008-11-24 12:29:48 +0000

    Method to get a handle to a file in the database, and a test that it works :-)
    git-svn-id: svn+ssh://romana.hawkins.id.au/home/justin/repos/CollabIRCate/trunk@159 8d8e6d39-c747-0410-aa99-ea999ebef502

--- lib/CollabIRCate/Schema/File.pm
+++ lib/CollabIRCate/Schema/File.pm
@@ -12,15 +12,16 @@ use CollabIRCate::Config;
 my $config = CollabIRCate::Config->config();
 
 my $store_path = $config->{file_store_path}
-  || croak "file_store_path not defined in config";
+    || croak "file_store_path not defined in config";
 
-croak "nowhere to store files - '$store_path' does not exist" unless -d $store_path;
+croak "nowhere to store files - '$store_path' does not exist"
+    unless -d $store_path;
 
 our %tmp_filenames;
 
 use base 'DBIx::Class';
 
-__PACKAGE__->load_components(qw/ PK::Auto Core/);
+__PACKAGE__->load_components(qw/PK::Auto Core/);
 __PACKAGE__->table("file");
 __PACKAGE__->add_columns(
     "id",
@@ -45,15 +46,15 @@ __PACKAGE__->add_columns(
 __PACKAGE__->set_primary_key("id");
 __PACKAGE__->add_unique_constraint( "file_pkey", ["id"] );
 
-
 sub new {
     my ( $class, $attrs ) = @_;
 
-    croak "no such file " . $attrs->{filename} unless (-e $attrs->{filename});
+    croak "no such file " . $attrs->{filename}
+        unless ( -e $attrs->{filename} );
 
-    if (! ref $attrs->{filename}) {
+    if ( !ref $attrs->{filename} ) {
         my $mime = MIME::Types->new();
-        my $type = $mime->mimeTypeOf($attrs->{filename});
+        my $type = $mime->mimeTypeOf( $attrs->{filename} );
         $attrs->{mime_type} = $type;
     }
     else {
@@ -61,10 +62,10 @@ sub new {
     }
 
     my $template = "$store_path/importXXXXXXXXXXX";
-    my $fh = File::Temp->new(TEMPLATE => $template, UNLINK => 0);
+    my $fh       = File::Temp->new( TEMPLATE => $template, UNLINK => 0 );
     my $filename = $fh->filename;
 
-    copy ($attrs->{filename}, $fh) || croak $!;
+    copy( $attrs->{filename}, $fh ) || croak $!;
     close $fh || croak $!;
 
     my $new = $class->next::method($attrs);
@@ -79,8 +80,7 @@ sub insert {
 
     my $mime   = MIME::Types->new();
     my $type   = $self->mime_type;
-    my $suffix = ( $mime->type($type)->extensions )[0];
-    my $dest   = sprintf( "%s/%08d.%s", _hash_dir($self->id), $self->id, $suffix);
+    my $dest = _filename($self->id);
     
     croak "file '$dest' already exists!" if -e $dest;
     rename $tmp_filenames{$self}, $dest;
@@ -88,18 +88,49 @@ sub insert {
     return $self;
 }
 
+
+sub fh {
+    my ( $self ) = shift;
+    my $id = $self->id;
+
+    my $fh;
+    open ($fh, "<", _filename($id)) || croak "could not open " . _filename($id);
+    return $fh;
+}
+
 sub _hash_dir {
     my $id = shift;
-    my $i = $id % 10;
-    my $j = ($id /10) % 10;
+    my $i  = $id % 10;
+    my $j  = ( $id / 10 ) % 10;
 
-    unless (-d "$store_path/$j") {
+    unless ( -d "$store_path/$j" ) {
         mkdir "$store_path/$j" || croak "could not mkdir: $!";
     }
-    unless (-d "$store_path/$j/$i") {
+    unless ( -d "$store_path/$j/$i" ) {
         mkdir "$store_path/$j/$i" || croak "could not mkdir: $!";
     }
 
     return "$store_path/$j/$i";
 }
+
+sub _filename {
+    my $id = shift;
+    return sprintf( "%s/%08d", _hash_dir( $id ), $id );
+}
+    
 1;
+
+__END__
+
+=head1 NAME
+
+CollabIRCate::Schema::File - Deal with files in the CollabIRCate system
+
+=head1 SYNOPSIS
+
+  my $set = $schema->resultset('File');
+  my $file = $schema->create ( { filename => '/tmp/somefile.txt' } );
+
+  my $id = $file->id;
+
+=cut
--- t/File.t
+++ t/File.t
@@ -1,12 +1,12 @@
 use strict;
 use warnings;
-use Test::More tests => 4;
+use Test::More tests => 6;
 
 use File::Temp qw/tempfile/;
 
 use CollabIRCate::Config;
 
-my $schema = CollabIRCate::Config->schema;
+my $schema   = CollabIRCate::Config->schema;
 my $template = "TESTXXXXXXXXXX";
 
 ok( defined $schema, 'got schema object' );
@@ -16,34 +16,49 @@ ok( defined $files, 'got some File resultset' );
 
 # test storing some files
 {
-    my ($fh, $filename) = tempfile($template);
+    my ( $fh, $filename ) = tempfile($template);
     rename $filename, "$filename.txt";
     $filename .= ".txt";
 
     my $files = $schema->resultset('File');
-    my $file = $files->create(
-                              {   filename  => $filename,
-                             }
-                            );
-  
+    my $file = $files->create( { filename => $filename, } );
+
     ok( defined $file->id && $file->id > 0, 'got a good file id, from file' );
+    unlink($filename);
 }
 
 SKIP: {
-    skip "mime types for filehandles not handled yet",1;
-  
-    my ($fh, $filename) = tempfile($template);
+    skip "mime types for filehandles not handled yet", 1;
+
+    my ( $fh, $filename ) = tempfile($template);
     rename $filename, "$filename.txt";
     $filename .= ".txt";
 
     my $files = $schema->resultset('File');
-    my $file = $files->create(
-                              {   filename  => $fh,
-                             }
-                            );
-    
-    ok( defined $file->id && $file->id > 0, 'got a good file id, from filehandle' );
+    my $file = $files->create( { filename => $fh, } );
+
+    ok( defined $file->id && $file->id > 0,
+        'got a good file id, from filehandle'
+    );
 
 }
 
+# test getting a files content back
+{
+    my ( $fh, $filename ) = tempfile($template);
+    rename $filename, "$filename.txt";
+    $filename .= ".txt";
+    print $fh "abc123\n";
+    close $fh;
 
+    my $files = $schema->resultset('File');
+    my $file = $files->create( { filename => $filename, } );
+
+    ok( defined $file->id && $file->id > 0, 'got a good file id, from file' );
+    unlink($filename);
+
+    my $fh_back = $file->fh;
+
+    my $data    = <$fh_back>;
+    ok( $data =~ /abc123/, 'got data back, via fh' );
+}
