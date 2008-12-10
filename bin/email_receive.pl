a62fbefa6ac47ea57e8be761d6ceb00057fac68a
justin <justin@8d8e6d39-c747-0410-aa99-ea999ebef502>
2008-12-10 13:26:28 +0000

    This is pretty hideous yet.
    Takes a MIME message on stdin, if it find a hash in the To: field (via
    plus addressing), any non text/plain attachments will be added to the
    CollabIRCate database.
    
    Still lots to do, including a horendous amount of test harness work
    to properly test this. I think I'll just hack it together first :-)
    
    
    
    git-svn-id: svn+ssh://romana.hawkins.id.au/home/justin/repos/CollabIRCate/trunk@174 8d8e6d39-c747-0410-aa99-ea999ebef502

---
+++ bin/email_receive.pl
@@ -0,0 +1,88 @@
+#!/usr/bin/perl
+
+use strict;
+use warnings;
+
+use FindBin qw/$Bin/;
+use Path::Class;
+use lib dir( $Bin, '..', 'lib' )->stringify;
+
+use Carp;
+use Pod::Usage;
+use Getopt::Long;
+use MIME::Parser;
+use File::Temp qw/tempfile/;
+
+use CollabIRCate::Config;
+use CollabIRCate::File qw/accept_file/;
+
+my $config = CollabIRCate::Config->config();
+my $schema = CollabIRCate::Config->schema();
+
+my ($debug, $help);
+
+my $options_okay = GetOptions(
+    'debug'     => \$debug,
+    'help'      => \$help,
+);
+
+if ( $help ) {
+    pod2usage( -exitval => 1 );
+    exit;    # unnecessary
+}
+
+### Create a new parser object:
+my $parser = new MIME::Parser;
+
+### Tell it where to put things:
+$parser->output_under("/tmp");
+
+my $entity = $parser->parse(\*STDIN);
+
+my $to = $entity->head->get('To');
+my ($hash) = $to =~ /\b([0-9a-f]{32,})\+/;
+
+croak "no hash in To: field" unless $hash;
+
+foreach my $this_entity ($entity->parts) {
+    if ($this_entity->effective_type ne 'text/plain')  {
+        my $head = $this_entity->head;
+        my $body = $this_entity->bodyhandle;
+        my $filename = $head->recommended_filename;
+        my $result;
+        eval {
+            $result = accept_file( $body->path, $hash );
+        };
+        die $@ if $@;
+        if ($result) {
+            warn "Accepted $filename into system\n";
+        }
+        else {
+            croak "oh I dunno!";
+        }
+    }
+}
+
+exit;
+
+__END__
+
+=head1 NAME
+
+email_receive.pl - Receive incoming files from email
+
+=head1 SYNOPSIS
+
+
+
+=head1 VERSION
+
+This documentation refers to version 0.0.1
+
+=head1 USAGE
+
+
+
+=head1 REQUIRED ARGUMENTS
+
+  
