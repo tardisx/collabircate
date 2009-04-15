6d71fcc0a3ce2d94679b6b31da25275782b90c3e
Justin Hawkins <justin@hawkins.id.au>
2009-04-15 16:36:14 +0930

    More documentation cleanups (so META.yml is nicer)

--- lib/CollabIRCate.pm
+++ lib/CollabIRCate.pm
@@ -12,7 +12,7 @@ use Catalyst::Runtime '5.70';
 #         -Debug: activates the debug mode for very useful log messages
 #   ConfigLoader: will load the configuration from a Config::General file in the
 #                 application's home directory
-# Static::Simple: will serve static files from the application's root 
+# Static::Simple: will serve static files from the application's root
 #                 directory
 
 use parent qw/Catalyst/;
@@ -21,7 +21,7 @@ use Catalyst qw/-Debug
                 Static::Simple/;
 our $VERSION = '0.02';
 
-# Configure the application. 
+# Configure the application.
 #
 # Note that settings in collabircate.conf (or other external
 # configuration file that you set up manually) take precedence
@@ -38,7 +38,7 @@ __PACKAGE__->setup();
 
 =head1 NAME
 
-CollabIRCate - Catalyst based application
+CollabIRCate - free, open-source collaboration tool
 
 =head1 SYNOPSIS
 
@@ -46,7 +46,18 @@ CollabIRCate - Catalyst based application
 
 =head1 DESCRIPTION
 
-[enter your description here]
+CollabIRCate is a free, open-source collaboration tool. It leverages
+existing services and environments, IRC, mail, the web, rather than
+trying to force you to learn Yet Another Tool.
+
+It is completely self-contained, you need nothing but a unix-like
+system with perl to run it on. The package contains an IRC server, but
+you can run it on an existing IRC server if you wish to use your
+existing infrastructure, or a public server.
+
+The software provides a powerful way to share ideas with others, using
+a unique combination of both instant and more traditional messaging
+technologies. Participation is via standard, intuitive techniques.
 
 =head1 SEE ALSO
 
