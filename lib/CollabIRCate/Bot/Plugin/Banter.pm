50a4ac832031437e603b0c9cca6a7d51945f4574
Justin Hawkins <justin@hawkins.id.au>
2009-04-27 22:01:55 +0930

    Thanks, bot. Thot.

--- lib/CollabIRCate/Bot/Plugin/Banter.pm
+++ lib/CollabIRCate/Bot/Plugin/Banter.pm
@@ -6,40 +6,52 @@ use warnings;
 use base 'CollabIRCate::Bot::Plugin';
 
 sub answer {
-  my $class    = shift;
-  my $question = shift;
-  my $args     = shift;
-  my $from     = $args->{from};
+    my $class    = shift;
+    my $question = shift;
+    my $args     = shift;
+    my $from     = $args->{from};
 
-  if ( $question =~ /fuck off/i
-    || $question =~ /get fucked/i
-    || $question =~ /hate.*you/
-    || $question =~ /you.*are.*(stupid|dumb|ugly)/ )
-  {
-    return { answer => "same to you $from" };
-  }
+    if (   $question =~ /fuck off/i
+        || $question =~ /get fucked/i
+        || $question =~ /hate.*you/
+        || $question =~ /you.*are.*(stupid|dumb|ugly)/ )
+    {
+        return { answer => "same to you $from" };
+    }
 
-  if ($question =~ /\b(hello|greetings|g'day|gday|hi)\b/) {
-    return { answer => "hello $from" };
-  }
+    if ( $question =~ /\b(hello|greetings|g'day|gday|hi)\b/ ) {
+        return { answer => "hello $from" };
+    }
 
-  if ($question =~ /\b(bye|goodbye|ciao)\b/) {
-    return { answer => "goodbye $from" };
-  }
+    if ( $question =~ /\b(bye|goodbye|ciao)\b/ ) {
+        return { answer => "goodbye $from" };
+    }
 
-  if ($question =~ /\b(how are you)\b/) {
-    return { answer => "just fine thanks $from" };
-  }
+    if ( $question =~ /\b(how are you)\b/ ) {
+        return { answer => "just fine thanks $from" };
+    }
 
-  if ($question =~ /\b(your mother)\b/) {
-    return { answer => "let me tell you about my mother *BLAM*" };
-  }
+    if ( $question =~ /\b(your mother)\b/ ) {
+        return { answer => "let me tell you about my mother *BLAM*" };
+    }
 
-  if ($question =~ /good.*(morning|afternoon|evening|night)/) {
-    return { answer => "and a very good $1 to you to" };
-  }
+    if ( $question =~ /good.*(morning|afternoon|evening|night)/ ) {
+        return { answer => "and a very good $1 to you to" };
+    }
 
-  return undef;
+    if ( $question =~ /thanks,?\s*(\w+)(.*?)/ ) {
+        my $word = $1;
+        my $answer;
+        my ( $first_bit, $last_bit ) = $word =~ /^([^aeiou]+)(.*)$/;
+        if ($last_bit) {
+            $answer = "th$last_bit";
+        }
+        else {
+            $answer = "th$word";
+        }
+        return { answer => $answer };
+    }
+    return undef;
 }
 
 1;
