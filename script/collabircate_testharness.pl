af0b87713234941ffb0dd6e53f832963b858dc66
Justin Hawkins <justin@hawkins.id.au>
2009-04-28 10:16:46 +0930

    Improve test harness, add command line options and better cleanup.

--- script/collabircate_testharness.pl
+++ script/collabircate_testharness.pl
@@ -2,11 +2,20 @@
 
 use strict;
 use warnings;
+use Getopt::Long;
+use Pod::Usage;
+
+my $shell;
+my $help;
+my $result = GetOptions("shell"  => \$shell,  # shell after start
+                        "help"   => \$help);  # help
+if (! $result) { pod2usage(2); exit; }
+if ($help)     { pod2usage(1); exit; }
 
 my $store = '/tmp/store' . $$;
 my $queue = '/tmp/queue' . $$;
-
 my $db = "/tmp/db" . $$;
+
 my $dsn   = "dbi:SQLite:dbname=$db";
 
 $ENV{COLLABIRCATE_CONFIG_FILE_STORE_PATH} = $store;
@@ -20,7 +29,10 @@ system ("cp", "collabircate.conf.sample", "collabircate.conf");
 mkdir $store;
 mkdir $queue;
 
+# set up
 system ("perl", "Makefile.PL");
+
+# deploy sql
 system ("bin/deploy.pl");
 
 # start the server and bot
@@ -28,8 +40,10 @@ my $spid = start_server();
 my $bpid = start_bot();
 sleep 5;
 
-if (@ARGV) {
+if ($shell) {
+  print "Escaping to shell - remember to exit after testing\n";
   system ($ENV{SHELL});
+  print "Killing servers and cleaning up\n";
 }
 else {
   system ("make", "test");
@@ -60,10 +74,25 @@ END {
   if (-e "collabircate.conf.preserved") {
     system ("mv", "collabircate.conf.preserved", "collabircate.conf");
   }
-  else {
+  elsif (-e "collabircate.conf") {
     system ("rm", "collabircate.conf");
   }
-  system ("rm", $db);
-  system ("rm", "-rf", $store);
-  system ("rm", "-rf", $queue);
+  system ("rm", $db) if ($db && -e $db);
+  system ("rm", "-rf", $store) if ($store && -d $store);
+  system ("rm", "-rf", $queue) if ($queue && -d $queue);
+  print "Cleanup complete\n";
 }
+
+__END__
+
+=head1 NAME
+
+collabircate_testharnesspl - Using GetOpt::Long and Pod::Usage
+
+=head1 SYNOPSIS
+
+  script/collabircate_testharness.pl [options]
+
+ Options:
+  --help            brief help message
+  --shell           escape to shell after setting up server and bot
