624413cb00ae5f46b130c561a9726323897a8d7f
Justin Hawkins <justin@hawkins.id.au>
2009-11-13 13:31:24 +1030

    Bit of lunchtime documentation.

--- lib/CollabIRCate/Bot/Plugin.pm
+++ lib/CollabIRCate/Bot/Plugin.pm
@@ -3,6 +3,41 @@ package CollabIRCate::Bot::Plugin;
 use strict;
 use warnings;
 
+=head1 NAME
+
+CollabIRCate::Bot::Plugin - base class for plugins
+
+=head2 DESCRIPTION
+
+This base class does nothing useful except to hold this documenation
+which tells you how to construct your plugin.
+
+It is mandatory to provide a register() method, and any methods which
+you register() for.
+
+Your register() method returns a hashref, with one or more of the following
+keys:
+
+  public    => $coderef,
+  addressed => $coderef,
+  periodic  => [ 60, $coderef ],
+
+The C<public> value is a reference to a subroutine to run when the Bot
+sees any message in any channel it is on. 
+
+The C<addressed> value is a reference to a subroutine to run when the Bot
+is specifically addressed, either publically or privately.
+
+The C<periodic> value is an arrayref containing two values, the first is
+the period (in seconds) determining how often the second coderef is actually
+run.
+
+As an example using all three of these, a plugin to collect per-channel
+statistics would need to register a C<public> method to collect line by
+line stats, and an C<addressed> method to answer questions about stats.
+
+=cut
+
 use Carp qw/croak/;
 
 # base class for plugins
