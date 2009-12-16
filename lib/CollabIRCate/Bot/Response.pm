42268ebfff7388832f86b71e1633d7dba5be78fb
Justin Hawkins <justin@hawkins.id.au>
2009-12-16 21:21:43 +1030

    Flesh out the bot response class and test class more.

--- lib/CollabIRCate/Bot/Response.pm
+++ lib/CollabIRCate/Bot/Response.pm
@@ -1,12 +1,44 @@
 package CollabIRCate::Bot::Response;
 
 use Moose;
+use Carp qw/croak/;
 
 # The response that a bot makes may involve zero or more channels, and
 # zero or more private messages. This package encapsulates such responses.
 
+has 'public_response'  => ( is => 'rw', isa => 'ArrayRef' );
+has 'private_response' => ( is => 'rw', isa => 'ArrayRef' );
 
-has 'public_response' => (is => 'rw', isa => 'ArrayRef');
-has 'private_response' => (is => 'rw', isa => 'ArrayRef');
+sub add_public_response {
+    my $self = shift;
+    my $args = shift || {};
 
+    my $channel = $args->{channel};
+    my $text    = $args->{text};
+
+    croak "no channel for public response" unless $channel;
+    croak "no text for public response"    unless $text;
+
+    my $current = $self->public_response || [];
+    $self->public_response( [ ( @{$current}, [ $channel, $text ] ) ] );
+    return $self;
+}
+
+sub add_private_response {
+    my $self = shift;
+    my $args = shift || {};
+
+    my $user = $args->{user};
+    my $text = $args->{text};
+
+    croak "no user for private response" unless $user;
+    croak "no text for private response" unless $text;
+
+    croak "not a CollabIRCate::Bot::Users object"
+        unless ( ref $user eq 'CollabIRCate::Bot::Users' );
+
+    my $current = $self->private_response || [];
+    $self->private_response( [ ( @{$current}, [ $user, $text ] ) ] );
+    return $self;
+}
 1;
---
+++ t/10unit/Bot-Response.t
@@ -0,0 +1,25 @@
+use Test::More tests => 6;
+
+use_ok 'CollabIRCate::Bot::Response';
+
+my $response = CollabIRCate::Bot::Response->new();
+ok ($response, 'response object');
+
+ok ($response->add_public_response({channel => '#foo', text => 'hello!'}), 'add public response');
+ok (@{$response->public_response} == 1, 'one public response');
+
+eval {
+    $response->add_private_response({text => 'hello!'});
+};
+ok ($@ =~ /no user for private/, 'no user');
+
+eval {
+    $response->add_private_response({text => 'hello!', user => 'fred'});
+};
+ok ($@ =~ /not a CollabIRCate::Bot::Users/, 'bad user');
+
+
+
+
+
+1;
