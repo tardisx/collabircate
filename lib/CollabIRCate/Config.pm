f5bc977eda8aebb62234903a6904b356b355fed2
Justin Hawkins <justin@hawkins.id.au>
2010-05-24 10:27:21 +0930

    Remove annoying warning message emitted by Config::Any

--- lib/CollabIRCate/Config.pm
+++ lib/CollabIRCate/Config.pm
@@ -6,7 +6,7 @@ use warnings;
 use Config::Any;
 
 sub config {
-    my $configs = Config::Any->load_files({files => ["collabircate.conf"]});
+    my $configs = Config::Any->load_files({files => ["collabircate.conf"], use_ext => 1});
     my $config = $configs->[0]->{'collabircate.conf'};
 
     return $config;
