87c0444b43fc67750f47733f8b765d949ee436b0
Justin Hawkins <justin@hawkins.id.au>
2010-05-29 21:49:49 +0930

    Commit the basic mojolicious framework additions.

---
+++ lib/CollabIRCate/Web.pm
@@ -0,0 +1,19 @@
+package CollabIRCate::Web;
+
+use strict;
+use warnings;
+
+use base 'Mojolicious';
+
+# This method will run once at server start
+sub startup {
+    my $self = shift;
+
+    # Routes
+    my $r = $self->routes;
+
+    # Default route
+    $r->route('/:controller/:action/:id')->to('example#welcome', id => 1);
+}
+
+1;
