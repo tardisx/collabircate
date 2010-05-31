e19d8b1ada69b41321cb78d6b5d36a526e019341
Justin Hawkins <justin@hawkins.id.au>
2010-05-31 21:30:36 +0930

    Make a / page.

--- lib/CollabIRCate/Web.pm
+++ lib/CollabIRCate/Web.pm
@@ -14,16 +14,15 @@ sub startup {
     # Routes
     my $r = $self->routes;
 
-    # Default route
-#    $r->route('/:controller/:action/:id')->to('example#welcome', id => 1);
-
     # user auth stuff
     $r->route('/user/login')->to('user#login');
     $r->route('/user/logout')->to('user#logout');
 
     # tokens
     $r->route('/token/:token')->to('token#index');
-    
+
+    # Default route
+    $r->route('/:controller/:action/:id')->to('root#welcome', id => 1);
 }
 
 1;
--- t/30web/basic.t
+++ t/30web/basic.t
@@ -11,4 +11,4 @@ use_ok('CollabIRCate::Web');
 # Test
 my $t = Test::Mojo->new(app => 'CollabIRCate::Web');
 $t->get_ok('/')->status_is(200)->content_type_is('text/html')
-  ->content_like(qr/Mojolicious Web Framework/i);
+  ->content_like(qr/Welcome/i);
