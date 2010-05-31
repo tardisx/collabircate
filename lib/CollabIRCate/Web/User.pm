4d84a7150c7697bc626748b87e4df4cd2433208f
Justin Hawkins <justin@hawkins.id.au>
2010-05-31 23:19:13 +0930

    Give login an actual form for entering values.

--- lib/CollabIRCate/Web/User.pm
+++ lib/CollabIRCate/Web/User.pm
@@ -10,35 +10,45 @@ use base 'Mojolicious::Controller';
 use CollabIRCate::DB::User;
 use CollabIRCate::DB::User::Manager;
 
+use HTML::FormFu;
+
 # Deal with logins (and logouts)
 sub login {
     my $self = shift;
+    my $form = HTML::FormFu->new({action => "/user/login"});
 
-    my $username = $self->req->param('username');
-    my $password = $self->req->param('password');
-
-    croak if (! $username || ! $password);
-
-    my $users = CollabIRCate::DB::User::Manager->get_users(
-        query => [ username => $username, password => $password ]
-    );
-
-    if ($users && $users->[0]) {
+    $self->stash->{message} = '';
     
-        # Render template "example/welcome.html.ep" with message
-        $self->stash->{message} = 'hi you logged in '. $username;
-        $self->stash->{session}->{logged_in} = $username;
+    $form->load_config_file('forms/login.yml');
+    $form->process($self->req->params->to_hash);
+
+    if ($form->submitted_and_valid) {
+        my $username = $self->req->param('username');
+        my $password = $self->req->param('password');
+
+        croak if (! $username || ! $password);
+
+        my $users = CollabIRCate::DB::User::Manager->get_users(
+            query => [ username => $username, password => $password ]
+        );
+        
+        if ($users && $users->[0]) {
+            $self->stash->{message} = 'login success';
+            $self->stash->{session}->{logged_in} = $username;
+        }
+        else {
+            $self->stash->{message} = 'login failure';
+            $self->stash->{session}->{logged_in} = undef;
+        }
     }
-    else {
-        $self->stash->{message} = 'you are so not logged in';
-        $self->stash->{session}->{logged_in} = undef;
-    }
+
+    $self->stash->{form} = $form;    
 }
 
 sub logout {
     my $self = shift;
-
-    $self->stash->{logged_in} = undef;
+    
+    $self->stash->{session}->{logged_in} = undef;
 }
 
 1;
