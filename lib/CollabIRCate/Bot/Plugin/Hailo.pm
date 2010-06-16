38f19170b0ef09f684632fa3c13861d937785934
Justin Hawkins <justin@hawkins.id.au>
2010-06-16 10:42:57 +0930

    Allow for a separate Halo DB per channel, and you can now request him to respond.

--- lib/CollabIRCate/Bot/Plugin/Hailo.pm
+++ lib/CollabIRCate/Bot/Plugin/Hailo.pm
@@ -10,16 +10,37 @@ use CollabIRCate::Logger;
 use base 'CollabIRCate::Bot::Plugin';
 
 my $logger = CollabIRCate::Logger->get(__PACKAGE__);
-my $hailo = Hailo->new(brain => 'hailo.db');    
+my $hailo_brains = {};
 my $last_channel = undef;
 my $channel_noise = {};
 
 sub register {
     return {
         public    => \&collect,
-        addressed => \&collect,
+        addressed => \&collect_and_emit,
         periodic  => [ 60, \&spurt ],
     };
+}
+
+sub collect_and_emit {
+    my $user     = shift;
+    my $channel  = shift;
+    my $text     = shift;
+
+    if ($text =~ /^say something/) {
+        my $hailo = _get_brain($channel);
+
+        my $response = CollabIRCate::Bot::Response->new;
+        $response->add_public_response(
+            {   channel => $channel,
+                text    => $hailo->reply(),  # this could supply $text
+            });
+        return $response;
+    }
+    else {
+        # just collect as usual
+        collect($user, $channel, $text);
+    }
 }
 
 sub collect {
@@ -28,8 +49,10 @@ sub collect {
     my $text     = shift;
 
     $last_channel = $channel;
-    
+
     $channel_noise->{$channel}++;
+
+    my $hailo = _get_brain($channel);
 
     $logger->debug("collecting '$text' for hailo");
     $hailo->learn($text);
@@ -45,7 +68,9 @@ sub spurt {
         my $chance = _noise_to_chance($channel_noise->{$channel});
         $channel_noise->{$channel} = 0;
         next if (rand(1) > $chance);
-    
+
+        my $hailo = _get_brain($channel);
+
         my $response = CollabIRCate::Bot::Response->new;
         $response->add_public_response(
             {   channel => $channel,
@@ -53,13 +78,13 @@ sub spurt {
             });
         $response->emit($irc);
     }
-    
+
     return;
 }
 
 sub _noise_to_chance {
     my $messages = shift;
-    
+
     my $noisy =      CollabIRCate::Config->config()->{plugin_hailo_noisy_messages} || 10;
     my $max_chance = CollabIRCate::Config->config()->{plugin_hailo_noisy_chance}   || 0.5;
     my $min_chance = CollabIRCate::Config->config()->{plugin_hailo_quiet_chance}   || 0.01;
@@ -71,8 +96,17 @@ sub _noise_to_chance {
 
     my $chance = $min_chance + $noise * $diff;
     return $chance;
-}    
-  
-    
-    
+}
+
+sub _get_brain {
+    my $channel = shift;
+    unless (CollabIRCate::Config->config()->{plugin_hailo_db_per_channel}) {
+        $channel = 'all';
+    }
+    return $hailo_brains->{$channel} if ( $hailo_brains->{$channel} );
+
+    $hailo_brains->{$channel} = Hailo->new(brain => 'hailo_' . $channel . '.db');
+    return $hailo_brains->{$channel};
+}
+
 1;
