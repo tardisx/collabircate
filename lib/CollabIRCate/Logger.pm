25388aec863ed46358b858a02b07410b0dc60c60
Justin Hawkins <justin@hawkins.id.au>
2011-04-01 10:01:56 +1030

    Documentation

--- lib/CollabIRCate/Logger.pm
+++ lib/CollabIRCate/Logger.pm
@@ -1,12 +1,33 @@
 package CollabIRCate::Logger;
 
+=head1 NAME
+
+CollabIRCate::Logger - a simple wrapper around Log::Log4perl
+
+=head2 SYNOPSIS
+
+  package CollabIRCate::Some::Class;
+
+  use CollabIRCate::Logger;
+
+  my $logger = CollabIRCate::Logger->get();
+  $logger->info("something informative");
+
+=cut
+
 use strict;
 use warnings;
 
 use Log::Log4perl;
 
 Log::Log4perl::init_and_watch('etc/log4perl.conf', 10);
+  ...
 
+=head2 get
+
+Returns the logger object.
+
+=cut
 
 sub get {
     my $class = shift;
