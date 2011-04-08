a1b109bcbf5838a961d69957bd98375d1ac67c6e
Justin Hawkins <justin@hawkins.id.au>
2011-04-08 11:21:09 +0930

    Start moving HTML fragment to the DB class. Enable thumboo thumbnail generation.

--- lib/CollabIRCate/DB/LogCombined.pm
+++ lib/CollabIRCate/DB/LogCombined.pm
@@ -2,10 +2,21 @@ package CollabIRCate::DB::LogCombined;
 
 use strict;
 use warnings;
+use URI::Find;
+use URI::Escape qw/uri_escape/;
+use LWP::Simple qw/get/;
+use CollabIRCate::Config;
 
 use Carp qw/croak/;
 
 use base 'CollabIRCate::DB::Object';
+
+# Get the config
+my $config = CollabIRCate::Config->config();
+# Create our URI Finder
+my $uri_finder = URI::Find->new(\&_process_uri);
+# And a global for the thumbnail data
+my @url_html;
 
 __PACKAGE__->meta->setup(
     table => 'log_combined',
@@ -60,5 +71,49 @@ sub nice_ts {
     my $ts = $self->ts;
     return $ts->strftime("%F %H:%M");
 }
+
+=head2 output_html
+
+Output this row as a HTML fragment.
+
+=cut
+
+sub output_html {
+    my $self = shift;
+    @url_html = ();
+    if ($self->entry_type eq 'log') {
+        # log stuff
+        my $output = $self->nice_ts . ": ";
+        my $text   = $self->entry;
+        $uri_finder->find(\$text);
+        $output .= $text . "<br />";
+        if (@url_html) { 
+          $output .= "<span>" . join('', @url_html) . "</span>";
+        }
+        return $output;
+    }
+    elsif ($self->entry_type eq 'file') {
+        die "dunno yet";
+    }
+    else {
+        die "can't handle " . $self->entry_type;
+    }
+}
+
+
+sub _process_uri {
+    my ($uri) = shift;
+
+    if ($config->{http_thumboo_api}) {
+      my $thumboo_api = $config->{http_thumboo_api};
+      my $thumoo_params = "u=".uri_escape("http://collabircate.eatmorecode.com/").
+                          "&su=".uri_escape($uri)."&c=medium&api=".$thumboo_api;
+      my $url = "http://counter.goingup.com/thumboo/snapshot.php?".$thumoo_params;
+      push @url_html, get $url;
+    }
+
+    return "<a href=\"$uri\">$uri</a>";
+}
+
 
 1;
